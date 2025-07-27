f676031522363b121ee9421c7a37f0370327b969
Khoi Nguyen <nguyenknkn76@gmail.com>
2025-07-27 17:02:47 +0700

    feat: handle success res

---
+++ backend/docs/howtodoit.md
@@ -0,0 +1,22 @@
+# HOW TO DO IT?
+
+## SETUP
+
+## ACCOUNT
+
+### SIGNUP
+
+- Using RSA
+- Common flows:
+
+### AUTHENTICATION
+
+### LOGIN
+
+### LOGOUT
+
+### Handle illegal TOKEN
+
+## PRODUCT
+
+### GENERAL
--- backend/src/controllers/access.controller.js
+++ backend/src/controllers/access.controller.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const { CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
   
   signUp = async (req, res, next) => {
-      console.log(`cmt.access.controller::signUp::`, req.body);
-      return res.status(201).json(await AccessService.signUp(req.body));
+      // console.log(`cmt.access.controller::signUp::`, req.body);
+      new CREATED ({
+        message: "Register OK",
+        metadata: await AccessService.signUp(req.body),
+        options: {
+          limit: 10,
+        }
+      }).send(res);
+      // return res.status(201).json(await AccessService.signUp(req.body));
   }
 }
 
